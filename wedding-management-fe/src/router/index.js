e921218a212e1496f0c3f4403ef5dad796c8d215
quoclongnguyen2003 <quoclongnguyen2003@gmail.com>
2025-05-21 09:26:01 +0700

    add lismenu

--- wedding-management-fe/src/router/index.js
+++ wedding-management-fe/src/router/index.js
@@ -3,6 +3,11 @@ import Home from '@/layout/Home/Home.vue'
 import Login from '@/components/page/Login/Login.vue'
 import Register from '@/components/page/Register/Register.vue'
 import Profile from '@/components/page/Profile/Profile.vue'
+import ListHall from '@/components/page/ListHall/ListHall.vue'
+import ListBranch from '@/components/page/ListBranch/ListBranch.vue'
+import ListMenu from '@/components/page/ListMenu/ListMenu.vue'
+
+
 
 
 const routes = [
@@ -14,6 +19,10 @@ const routes = [
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
   { path: '/profile', name: 'Profile', component: Profile },
+  { path: '/listHall', name: 'ListHall', component: ListHall },
+  { path: '/listbranch', name :'ListBranch', component: ListBranch},
+  { path: '/listmenu', name :'ListMenu', component: ListMenu},
+
  
 
  
